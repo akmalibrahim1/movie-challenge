b161a574cb3e6b87d1a0d4f868eda01156a91f26
akmal <akmalibrahim1@gmail.com>
2021-02-17 02:21:32 +0000

    Add methods to make api calls

---
+++ src/util/omdb.js
@@ -0,0 +1,35 @@
+const dataRequests = "http://www.omdbapi.com/?apikey="+ process.env.REACT_APP_API_KEY +"&"
+const posterRequest= "http://img.omdbapi.com/?apikey="+ process.env.REACT_APP_API_KEY +"&"
+const axios = require("axios")
+
+//General api promise call 
+function makeOmdbCall(url){
+    return new Promise(function (resolve, reject) { 
+        axios.get(url).then( 
+            (response) => { 
+                var result = response.data; 
+                resolve(result); 
+            }, 
+                (error) => { 
+                reject(error); 
+            } 
+        ); 
+    });
+}
+
+export const searchTypesList = ["All", "Movie", "Series"]
+
+//Retrieve movie by name
+export function searchMovieByName(movieName){
+    return makeOmdbCall(dataRequests + "t=" +movieName)
+}
+
+//Retrieve movie by imdbID
+export function searchById(movieId){
+    return makeOmdbCall(dataRequests + "i=" +movieId)
+}
+
+//Retrieve the results for a list of movies/shows (paginated)
+export function searchMovieAndSeries(title, pageNumber){
+    return makeOmdbCall(dataRequests + "s=" + title + "&page=" + pageNumber)
+}
