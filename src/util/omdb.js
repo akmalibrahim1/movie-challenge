f381aa1ae983c79da2e15742884a28e24c657844
akmal <akmalibrahim1@gmail.com>
2021-02-20 02:22:26 +0000

    Add method to create link to imdb reference

--- src/util/omdb.js
+++ src/util/omdb.js
@@ -1,5 +1,6 @@
 const dataRequests = "http://www.omdbapi.com/?apikey="+ process.env.REACT_APP_API_KEY +"&"
 const posterRequest= "http://img.omdbapi.com/?apikey="+ process.env.REACT_APP_API_KEY +"&"
+const imdbLink= "https://www.imdb.com/title/"
 const axios = require("axios")
 
 //General api promise call 
@@ -32,4 +33,8 @@ export function searchById(movieId){
 //Retrieve the results for a list of movies/shows (paginated)
 export function searchMovieAndSeries(title, pageNumber){
     return makeOmdbCall(dataRequests + "s=" + title + "&page=" + pageNumber)
+}
+
+export function getImdbLink(id){
+    return imdbLink + id
 }
